fb187939b7424ad113fe8089188a92db3c30c0c1
Isa Memmedli <i.a.memmedli@gmail.com>
2023-07-03 02:56:20 +0400

    Add project files.

---
+++ WebApplication1/Middlewares/LoginMiddleware.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Middlewares;
+
+public class LoginMiddleware
+{
+    private RequestDelegate? _next;
+
+    public LoginMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        Console.WriteLine("Login started");
+        await _next?.Invoke(context);
+        Console.WriteLine("Login ended");
+    }
+}
